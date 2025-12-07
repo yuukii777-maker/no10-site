774e91251da9f7506231138ecd7f291b2c490a02
yuukii777 <yuuki777@icloud.com>
2025-12-08 00:39:02 +0900

    fix: tailwind and postcss configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
 
-const config: Config = {
+export default {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
@@ -9,7 +9,4 @@ const config: Config = {
   theme: {
     extend: {},
   },
-  plugins: [],
-};
-
-export default config;
+} satisfies Config;
